ce1317c173683c605d5b85b8c48e6c2329613745
30james00 <48206982+30james00@users.noreply.github.com>
2022-10-07 23:04:48 +0200

    Init repo

---
+++ Lab1/src/main/java/com/mstolarz/lab1/HelloServlet.java
@@ -0,0 +1,98 @@
+package com.mstolarz.lab1;
+
+import java.io.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    private Date data1;
+    private final DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+    public void init() {
+        data1 = new Date();
+    }
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet HelloServerlet</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet HelloServerlet at " + request.getContextPath() + "</h1>");
+            out.println("<p>data z init " + dateFormat.format(data1) + "</p>");
+            out.println("<p>data z processRequest " + dateFormat.format(new Date()) + "</p>");
+            out.println("<h2>Dane serwera</h2>");
+            out.println("<p>request.getServerName(): " + request.getServerName() + "</p>");
+            out.println("<p>request.getServerPort(): " + request.getServerPort() + "</p>");
+            out.println("<p>request.getRemoteHost(): " + request.getRemoteHost() + "</p>");
+            out.println("<p>request.getRemoteAddr(): " + request.getRemoteAddr() + "</p>");
+            out.println("<h2>Szczegóły żądania</h2>");
+            out.println("<p>request.getMethod(): " + request.getMethod() + " </p>");
+            out.println("<p>request.getQueryString(): " + request.getQueryString() + "</p>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request  servlet request
+     * @param response servlet response
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request  servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException      if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+    public void destroy() {
+    }
+}
