a3647e1094291519dd8f042b134af50097289478
30james00 <48206982+30james00@users.noreply.github.com>
2022-11-07 01:28:23 +0100

    Lab 3_3

--- Lab3/src/main/java/com/mstolarz/lab3/ListServlet.java
+++ Lab3/src/main/java/com/mstolarz/lab3/ListServlet.java
@@ -7,35 +7,43 @@ import jakarta.servlet.annotation.*;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.*;
+import java.util.ArrayList;
 
 @WebServlet(name = "ListServlet", value = "/ListServlet")
 public class ListServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        try (PrintWriter out = response.getWriter()) {
-            try {
-                Class.forName("com.mysql.cj.jdbc.Driver");
-            } catch (ClassNotFoundException e) {
-                throw new RuntimeException(e);
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        try {
+            //utworzenie obiektu połączenia do bazy danych MySQL:
+            Connection conn;
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/world?serverTimezone = UTC", "root", "student");
+            //utworzenie obiektu do wykonywania zapytań do bd:
+            Statement st;
+            st = conn.createStatement();
+            String query = "SELECT * FROM country WHERE continent = 'Europe'";
+            //wykonanie zapytania SQL:
+            ResultSet rs;
+            HttpSession session = request.getSession(true);
+            CountryBean country;
+            ArrayList<CountryBean> countryList = new ArrayList<CountryBean>();
+            rs = st.executeQuery(query);
+            while (rs.next()) {
+                //pobierz i wyświetl dane z odpowiedniej kolumny
+                country = new CountryBean();
+                country.setCode(rs.getString("code"));
+                country.setName(rs.getString("name"));
+                country.setPopulation(rs.getInt("population"));
+                countryList.add(country);
             }
-            try {
-                //utworzenie obiektu połączenia do bazy danych MySQL:
-                Connection conn;
-                conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/world?serverTimezone = UTC", "root", "student");
-                //utworzenie obiektu do wykonywania zapytań do bd:
-                Statement st;
-                st = conn.createStatement();
-                String query = "SELECT * FROM country WHERE continent = 'Europe'";
-                //wykonanie zapytania SQL:
-                ResultSet rs;
-                rs = st.executeQuery(query);
-                while (rs.next()) {
-                    //pobierz i wyświetl dane z odpowiedniej kolumny
-                    out.print(rs.getString("name"));
-                }
-            } catch (SQLException e) {
-                throw new RuntimeException(e);
-            }
+            session.setAttribute("list", countryList);
+            response.sendRedirect("countryList.jsp");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 
---
+++ Lab3/src/main/webapp/countryList.jsp
@@ -0,0 +1,21 @@
+<%@ page import="java.util.ArrayList" %>
+<%@ page import="com.mstolarz.lab3.CountryBean" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Country List</title>
+</head>
+<body>
+<% ArrayList<CountryBean> countryList = (ArrayList<CountryBean>) session.getAttribute("list");
+    for (CountryBean country : countryList) { %>
+<p><%= country.getCode()%>
+</p>
+<p><%= country.getName()%>
+</p>
+<p><%= country.getPopulation()%>
+</p>
+<%
+    }
+%>
+</body>
+</html>
