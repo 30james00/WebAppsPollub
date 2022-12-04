2b7b7d95a0a2cce2e35de7a1db94da8f40948f12
30james00 <48206982+30james00@users.noreply.github.com>
2022-12-04 21:53:32 +0100

    Lab 7 Init

--- Lab6/pai_security/src/main/java/com/mstolarz/pai_security/controllers/UserController.java
+++ Lab6/pai_security/src/main/java/com/mstolarz/pai_security/controllers/UserController.java
@@ -9,12 +9,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.security.Principal;
 import java.util.Objects;
-import java.util.logging.Logger;
 
 @Controller
 public class UserController {
--- Lab6/pai_security/src/main/java/com/mstolarz/pai_security/entity/User.java
+++ Lab6/pai_security/src/main/java/com/mstolarz/pai_security/entity/User.java
@@ -2,7 +2,6 @@ package com.mstolarz.pai_security.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 @Entity
@@ -72,11 +71,8 @@ public class User {
         return password;
     }
 
-
     public void setPassword(String password) {
         this.password = password;
-
-
     }
 
     @Override
